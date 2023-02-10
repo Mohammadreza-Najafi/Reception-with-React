ac0a9eb1da5bff991d81b02535cc5b9bf8784ea5
Mohammadreza Najafi <mohammadreza.najafi.2002@gmail.com>
2023-02-10 17:48:17 +0330

    feat: Add About component with Lorem Ipsum text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,8 @@ const About = ()=>{
 
     return (
         <section className="section about-section">
-            <h1 className=>about us</h1>
+            <h1 className="section-title">about us</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto autem blanditiis consequuntur dolor doloribus dolorum earum eius error facilis, fuga ipsam minus natus nemo placeat quae, recusandae sapiente. Assumenda aut facilis illum laboriosam necessitatibus nobis odit perspiciatis repudiandae sint voluptatem!</p>
         </section>
     )
 }
