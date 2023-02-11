6eae39e9927fa4ee4c0c850b31ef27650e2c0871
Mohammadreza Najafi <mohammadreza.najafi.2002@gmail.com>
2023-02-11 14:38:19 +0330

    feat:start implement ReceptionList component and this comment , add receptions,loading states and import Loading component

--- src/component/ReceptionList.jsx
+++ src/component/ReceptionList.jsx
@@ -1,5 +1,17 @@
+import {useGlobalContext} from "../context.jsx";
+import Loading from "./Loading.jsx";
+
 const ReceptionList=() => {
 
+    const {receptions,loading} = useGlobalContext()
+    if(loading){
+        return <Loading/>
+    }
+    return(
+        <div>
+            <h2>reception list</h2>
+        </div>
+    )
 
 }
 
