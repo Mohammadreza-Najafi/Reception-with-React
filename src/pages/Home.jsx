d946f0697a10a6c493b59fda70e4a234c39c883d
Mohammadreza Najafi <mohammadreza.najafi.2002@gmail.com>
2023-02-11 14:26:32 +0330

    feat:Add SearchForm and ReceptionList component to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,13 @@
+import ReceptionList from "../component/ReceptionList.jsx";
+import SearchForm from "../component/SearchForm.jsx";
+
 const Home= () =>{
+    return (
+        <main>
+            <SearchForm/>
+            <ReceptionList/>
+        </main>
+    )
 
 }
 export default Home
