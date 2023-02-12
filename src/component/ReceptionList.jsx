b1c9763709c32da1e9f1b2ede88ef792fee96463
Mohammadreza Najafi <mohammadreza.najafi.2002@gmail.com>
2023-02-12 16:04:44 +0330

    feat:complete implementation ReceptionList.

--- src/component/ReceptionList.jsx
+++ src/component/ReceptionList.jsx
@@ -1,18 +1,33 @@
 import {useGlobalContext} from "../context.jsx";
 import Loading from "./Loading.jsx";
+import Reception from "./Reception.jsx";
 
 const ReceptionList=() => {
 
     const {receptions,loading} = useGlobalContext()
+
     if(loading){
         return <Loading/>
     }
-    return(
-        <div>
-            <h2>reception list</h2>
-        </div>
-    )
 
+    if(receptions.length < 1){
+        return (
+            <h2 className="section-title">
+                no receptions matched your search criteria
+            </h2>
+        )
+    }
+
+    return(
+        <section className="section">
+            <h2 className="section-title">reception list</h2>
+            <div className="cocktails-center">
+                {receptions.map((item) =>{
+                    return <Reception key={item.id} {...item} />
+                })}
+            </div>
+        </section>
+    )
 }
 
 export default ReceptionList
