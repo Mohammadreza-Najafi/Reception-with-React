1c40cb37f2342a2c6e72ebccc4a74391bb1dd18b
Mohammadreza Najafi <mohammadreza.najafi.2002@gmail.com>
2023-02-12 16:19:26 +0330

    feat: continue completion of context.Add fetchDrinks function for get data form server

--- src/context.jsx
+++ src/context.jsx
@@ -1,21 +1,61 @@
-import React,{useContext, useState} from "react";
-
+import React, { useState, useEffect,useContext,useCallback} from "react";
 
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 
 const AppContext = React.createContext()
 
 const AppProvider = ({children}) => {
-    const [loading,setLoading] = useState(true)
+    const [loading,setLoading] = useState(false)
     const [searchTerm,setSearchTerm] = useState('a')
-    const [receptions,setReceptions] = useState()
+    const [receptions,setReceptions] = useState([])
+
+    const fetchDrinks = async () =>{
+        // setLoading(true)
+        try {
+            const response = await fetch(`${url}${searchTerm}`)
+            const data = await response.json()
+            const {drinks} = data
+
+            if(drinks){
+                const tempReceptions = drinks.map((item) => {
+                    const {
+                        idDrink,
+                        strDrink,
+                        strDrinkThumb,
+                        strAlcoholic,
+                        strGlass
+                    }= item
+                    return {
+                        id: idDrink,
+                        name: strDrink,
+                        image: strDrinkThumb,
+                        info: strAlcoholic,
+                        glass: strGlass
+                    }
+                })
+                setReceptions(tempReceptions)
+            }else {
+                setReceptions([])
+            }//end of if
+
+            setLoading(false)
+
+        }catch (error){
+            setLoading(false)
+        }//end of try_catch
+
+    }
+
+    useEffect(() => {
+        fetchDrinks()
+    }, [searchTerm,fetchDrinks])
 
     return (
         <AppContext.Provider
             value={{
                 loading,
-                searchTerm,
                 receptions,
+                searchTerm,
                 setSearchTerm
             }}
         >
