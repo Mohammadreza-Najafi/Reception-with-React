11513959da814835056b891c4315999897d3ff02
Mohammadreza Najafi <mohammadreza.najafi.2002@gmail.com>
2023-02-13 20:10:55 +0330

    feat :implement SearchForm component. Used setSearchTerm state from context. Implement struct for search reception logic.

--- src/component/SearchForm.jsx
+++ src/component/SearchForm.jsx
@@ -1,4 +1,40 @@
-const SearchForm=() => {
+import React from "react";
+import {useGlobalContext} from "../context.jsx";
+
+const SearchForm = () => {
+
+    const {setSearchTerm} = useGlobalContext()
+
+    const searchValue = React.useRef('')
+
+    React.useEffect(() => {
+        searchValue.current.focus()
+    }, [])
+
+    const searchReception = () => {
+        setSearchTerm(searchValue.current.value)
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+    }
+
+    return (
+            <section className='section search'>
+                <form className='search-form' onSubmit={handleSubmit}>
+                    <div className="form-control">
+                        <label htmlFor="name"> search</label>
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            ref={searchValue}
+                            onChange={searchReception}
+                        />
+                    </div>
+                </form>
+            </section>
+    )
 
 
 }
